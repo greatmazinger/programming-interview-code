5c920789601518447cf3a7c82854fc1acb3b989f
Raoul Veroy <rveroy@cs.tufts.edu>
2014-10-15 14:37:06 -0400

    taking out try-except in sieve.py

--- Python/sieve.py
+++ Python/sieve.py
@@ -6,12 +6,8 @@ def sieve(xs):
     for y in sieve(i for i in xs if (i % x) > 0):
         yield y
 
-try:
-    for p in sieve(itertools.count(2)):
-        print p,
-        if p > 1000:
-            break
-    print ''
-except RuntimeError as e:
-    print e
-
+for p in sieve(itertools.count(2)):
+    print p,
+    if p > 1000:
+        break
+print ''
