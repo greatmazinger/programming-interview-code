bc6d3d710409b5801a7acedbd8b35ee558b1a862
Raoul Veroy <rveroy@cs.tufts.edu>
2014-10-23 01:00:57 -0400

    Add a version of dfs_iter2, that prints out the nodes in the same order as the recursive version.

--- Python/dfs.py
+++ Python/dfs.py
@@ -24,6 +24,24 @@ def dfs_iter( G, # graph
             for tgt in nx.all_neighbors(G, src):
                 stack.append(tgt)
 
+def dfs_iter2( G, # graph
+               discovered, # list of discovered flag, 1 to 1 with nodes in G
+               node # start at this node
+             ):
+    # Slightly more complicated that dfs_iter, but will produce
+    # the same order as dfs_rec.
+    stack = [ node ]
+    while len(stack) > 0:
+        src = stack.pop()
+        if not discovered[src]:
+            discovered[src] = 1
+            print src,
+            neighbors = reversed(list(nx.all_neighbors(G, src)))
+            # To get the same order as recursive, we reverse the
+            # order in which we insert the nodes.
+            for tgt in neighbors:
+                stack.append(tgt)
+
 G = nx.moebius_kantor_graph()
 print "Nodes:", G.nodes()
 print "Edges:", G.edges()
@@ -45,6 +63,13 @@ print "======================================================================"
 print "DFS (iterative) from %d" % root
 discovered = [ 0 for x in xrange(G.number_of_nodes()) ]
 dfs_iter( G, discovered, root )
+print
+
+print "======================================================================"
+
+print "DFS (iterative version 2) from %d" % root
+discovered = [ 0 for x in xrange(G.number_of_nodes()) ]
+dfs_iter2( G, discovered, root )
 print
 
 print "======================================================================"
