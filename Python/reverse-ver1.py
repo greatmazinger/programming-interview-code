3fe4b37aac3a98ddd0ef758f201ca26294ed4880
Raoul Veroy <rveroy@cs.tufts.edu>
2014-10-22 22:29:57 -0400

    Add a non-recursive version of reverse.

--- Python/reverse-ver1.py
+++ Python/reverse-ver1.py
@@ -14,6 +14,7 @@ def get_next_word( words ):
             result.append( words.pop(0) )
     return result           
 
+# Recursive reverse
 def reverse( words ):
     if len(words) == 0:
         return []
@@ -23,13 +24,37 @@ def reverse( words ):
     result.extend( myword )
     return result
 
+def reverse_iterative( words ):
+    if len(words) == 0:
+        return []
+    total = len(words)
+    result = []
+    seen = 0
+    while seen < total:
+        myword = get_next_word( words )
+        seen = seen + len(myword)
+        myword.extend( result )
+        result = myword
+    return result
+
 words = []
 words.extend( x for x in "I do not like them in a house I do not like them with a mouse    " )
 print "Original:"
 print words
-print "Reversed:"
+print "============================================================"
+print "Reverse using recursive version:"
 revwords = reverse(words)
 print "   as list:"
 print revwords
 print "   as string:"
 print "".join( x for x in revwords )
+print "============================================================"
+print "Original:"
+words.extend( x for x in "I do not like them in a house I do not like them with a mouse    " )
+print words
+print "Reverse using iterative version:"
+revwords2 = reverse_iterative(words)
+print "   as list:"
+print revwords2
+print "   as string:"
+print "".join( x for x in revwords2 )
