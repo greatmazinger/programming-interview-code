f3e247649f6f790b80cf297fa1d860d546c01d42
Raoul Veroy <rveroy@cs.tufts.edu>
2014-10-23 00:41:39 -0400

    Adding an iterative version of DFS. Question: How do i implement it so that it visits the vertices in the same order as the recursive? (Or vice versa)

--- Python/dfs.py
+++ Python/dfs.py
@@ -4,12 +4,25 @@ from random import randrange
 def dfs_rec( G, # graph
              discovered, # list of discovered flag, 1 to 1 with nodes in G
              node # start at this node
-             ):
+           ):
     discovered[node] = 1
     print node,
     for tgt in nx.all_neighbors(G, node):
         if not discovered[tgt]:
             dfs_rec( G, discovered, tgt )
+
+def dfs_iter( G, # graph
+              discovered, # list of discovered flag, 1 to 1 with nodes in G
+              node # start at this node
+            ):
+    stack = [ node ]
+    while len(stack) > 0:
+        src = stack.pop()
+        if not discovered[src]:
+            discovered[src] = 1
+            print src,
+            for tgt in nx.all_neighbors(G, src):
+                stack.append(tgt)
 
 G = nx.moebius_kantor_graph()
 print "Nodes:", G.nodes()
@@ -21,6 +34,17 @@ print "Discovered array before:", discovered
 root = randrange(0, G.number_of_nodes())
 print "Randomly select the root node:", root
 
-print "DFS from %d" % root
+print "======================================================================"
+
+print "DFS (recursive) from %d" % root
 dfs_rec( G, discovered, root )
 print
+
+print "======================================================================"
+
+print "DFS (iterative) from %d" % root
+discovered = [ 0 for x in xrange(G.number_of_nodes()) ]
+dfs_iter( G, discovered, root )
+print
+
+print "======================================================================"
