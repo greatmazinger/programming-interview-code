150f320ae3a57e688cc5202327b149f79029bc3b
Raoul Veroy <raoul@littletechtips.com>
2015-11-28 23:47:02 -0500

    Adding countprimes2.py. Adding some more ignore files.

---
+++ Python/countprimes2.py
@@ -0,0 +1,18 @@
+import itertools
+import fileinput
+
+def sieve(xs):
+    x = next(xs)
+    yield x
+    for y in sieve(i for i in xs if (i % x) > 0):
+        yield y
+
+if __name__ == "__main__":
+    count = 0
+    stdin = fileinput.input()
+    num = int(stdin.next())
+    for p in sieve(itertools.count(2)):
+        if p > num:
+            break
+        count += 1
+print count
