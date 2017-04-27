4fc78591078e01f46c260f7892feeecca15e2803
Raoul Veroy <raoul@littletechtips.com>
2017-04-27 02:14:43 -0400

    Save updated code using the wordcount

--- Python/basic_python/wordcount.py
+++ Python/basic_python/wordcount.py
@@ -3,10 +3,26 @@ import sys
 import os
 # Lots of important functionality in os. Most importantly
 # for us is the processing of files
+from collections import defaultdict
 
 def split_into_words( line ):
-    words = []
+    line = line.rstrip("\n")
+    words = line.split(" ")
     return words
+
+def clean_words( words ):
+    result = []
+    for w in words:
+        # Convert everything to lower case
+        w = w.lower()
+        # Remove all(?) punctuation marks
+        w = w.replace( ".", "" )
+        w = w.replace( ",", "" )
+        w = w.replace( "_", "" )
+        w = w.replace( ";", "" )
+        w = w.replace( ":", "" )
+        result.append( w )
+    return result
 
 def main():
     print "Running", sys.argv[0]
@@ -20,15 +36,19 @@ def main():
     if not os.path.isfile(filename):
         print "File %s not found." % filename
         exit(2)
+    counter = defaultdict( int )
     with open(filename, "rb") as filehandle:
         # "rb" means
         #  r - read mode
         #  b - binary mode. Don't worry too much about this, but
         #      you almost always want binary mode.
         for line in filehandle:
-            print line
             words = split_into_words( line )
-            print words
+            words = clean_words( words )
+            for x in words:
+                counter[x] = counter[x] + 1
+        for k, v in counter.iteritems():
+            print k, "->", v
 
 if __name__ == "__main__":
     main()
