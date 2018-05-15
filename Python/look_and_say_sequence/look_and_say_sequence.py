2f05898d4c523030789d9a7e33a701b63fb4b58a
Raoul Veroy <raoul@littletechtips.com>
2018-05-15 02:34:39 -0400

    Skeleton for look_and_say_sequence

---
+++ Python/look_and_say_sequence/look_and_say_sequence.py
@@ -0,0 +1,7 @@
+import fileinput
+
+if __name__ == "__main__":
+    stdin = fileinput.input()
+    num = int(stdin.next())
+    print "--------------------------------------------------------------------------------"
+    # TODO
