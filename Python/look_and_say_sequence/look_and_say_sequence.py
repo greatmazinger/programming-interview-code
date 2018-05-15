c467f5e9a1b44384800719788687c00ffb7bd0bc
Raoul Veroy <raoul@littletechtips.com>
2018-05-15 02:36:21 -0400

    Add some comments to the look and say sequence.

--- Python/look_and_say_sequence/look_and_say_sequence.py
+++ Python/look_and_say_sequence/look_and_say_sequence.py
@@ -1,5 +1,11 @@
 import fileinput
 
+"""
+Look and say sequence: 
+    To generate a member of the sequence from the previous member, read off the digits of the previous member.
+    Analyzed by John Conway.
+    from: https://en.wikipedia.org/wiki/Look-and-say_sequence
+"""
 if __name__ == "__main__":
     stdin = fileinput.input()
     num = int(stdin.next())
