35825092702db9e032422bb77b0a9a4eaa4a7ba7
Raoul Veroy <bveroy@gmail.com>
2023-08-01 22:29:33 -0700

    Add problem 2: Valid parens string

--- Python/02-valid-paren/valid-paren.py
+++ Python/02-valid-paren/valid-paren.py
@@ -1,29 +1,60 @@
-class Solution:
-    def is_open(self, ch: str) -> bool:
-        return ch in ["(", "{", "["]
+from typing import List
 
-    def check_if_valid(self, ch: str, stack: List[str]) -> bool:
-        if self.is_open(ch):
-            stack.append(ch)
-            return True
-            # If the new character is an open, then adding to the stack is always valid.
-        # So this is a closing paren. We have to check to see if it matches the top of the stack
-        result = False
-        if len(stack) > 0:
-            if ch == ")":
-                result = (stack[-1] == "(")
-            elif ch == "}":
-                result = (stack[-1] == "{")
-            elif ch == "]":
-                result = (stack[-1] == "[")
+def is_open(ch: str) -> bool:
+    return ch in ["(", "{", "["]
 
-            if result:
-                stack.pop()
-        return result
+def check_if_valid(ch: str, stack: List[str]) -> bool:
+    if is_open(ch):
+        stack.append(ch)
+        return True
+        # If the new character is an open, then adding to the stack is always valid.
+    # So this is a closing paren. We have to check to see if it matches the top of the stack
+    result = False
+    if len(stack) > 0:
+        if ch == ")":
+            result = (stack[-1] == "(")
+        elif ch == "}":
+            result = (stack[-1] == "{")
+        elif ch == "]":
+            result = (stack[-1] == "[")
 
-    def isValid(self, s: str) -> bool:
-        stack = []
-        for ch in s:
-            if not self.check_if_valid(ch, stack):
-                return False
-        return len(stack) == 0
+        if result:
+            stack.pop()
+    return result
+
+def isValid(s: str) -> bool:
+    stack = []
+    for ch in s:
+        if not check_if_valid(ch, stack):
+            return False
+    return len(stack) == 0
+
+def test1():
+    print("Running TEST 1:")
+    teststr = "()"
+    assert isValid(teststr)
+    print("  -> TEST 1 passed.")
+
+def test2():
+    print("Running TEST 2:")
+    teststr = "(){"
+    assert not isValid(teststr)
+    print("  -> TEST 2 passed.")
+
+def test3():
+    print("Running TEST 3:")
+    teststr = "]{}"
+    assert not isValid(teststr)
+    print("  -> TEST 3 passed.")
+
+def test4():
+    print("Running TEST 4:")
+    teststr = "(){}[]((({}{}[])))"
+    assert isValid(teststr)
+    print("  -> TEST 4 passed.")
+
+if __name__ == "__main__":
+    test1()
+    test2()
+    test3()
+    test4()
