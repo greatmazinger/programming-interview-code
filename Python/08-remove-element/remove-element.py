c9caf17dff1f44a9b133625c4aa5bbf90dd452ac
Raoul Veroy <bveroy@gmail.com>
2023-08-03 17:12:00 -0700

    Add problem 8: Remove element from list

---
+++ Python/08-remove-element/remove-element.py
@@ -0,0 +1,27 @@
+def removeElement(self, nums: List[int], val: int) -> int:
+    # look for not val at end -> cur
+    # while not val cur or cur > 0
+    # sptr at start, look for val
+    # when val found, swap sptr and cur values
+    if len(nums) == 0:
+        return 0
+    sptr = 0
+    cur = len(nums) - 1
+    while True:
+        # starting at cur, look for first non-val
+        while cur > 0 and nums[cur] == val:
+            cur -= 1
+        if cur == 0:
+            break
+        # cur is pointing to a non-val
+        while sptr < cur and nums[sptr] != val:
+            sptr += 1
+        if (sptr < cur) and nums[sptr] == val:
+            nums[sptr] = nums[cur]
+            nums[cur] = val
+        elif sptr >= cur:
+            break
+    k = 0
+    while k < len(nums) and nums[k] != val:
+        k += 1
+    return k
